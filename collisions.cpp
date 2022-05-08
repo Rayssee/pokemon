939bd7e9e5be9fadae760ba9de11e1eb130117eb
Asukyy <asukii@outlook.fr>
2022-05-08 12:47:20 +0200

    pokemon

--- collisions.cpp
+++ collisions.cpp
@@ -261,7 +261,7 @@ if((abs((sprite_perso.getPosition().x >=180 && sprite_perso.getPosition().x <=43
                         ssalon.setScale(0.f, 0.f);
                         schambre.setScale(2.f, 2.f);
                         sprite_perso.setPosition(250, 170);
-                        j=-1;
+
                     }
 
         if((sprite_perso.getPosition().x>=160 && (sprite_perso.getPosition().x<=260)) &&  (sprite_perso.getPosition().y==520) && j==-2){
@@ -272,4 +272,185 @@ if((abs((sprite_perso.getPosition().x >=180 && sprite_perso.getPosition().x <=43
             sprite_perso.setPosition(680,300);
         }
 
+
+                     if((abs((sprite_perso.getPosition().y >=150 && sprite_perso.getPosition().y <=530)&& (sprite_perso.getPosition().x ==160)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x+10, sprite_perso.getPosition().y);
+         }
+          if((abs((sprite_perso.getPosition().x >=170 && sprite_perso.getPosition().x <=710)&& (sprite_perso.getPosition().y ==530)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y-10);
+         }
+          if((abs((sprite_perso.getPosition().y >=220 && sprite_perso.getPosition().y <=520)&& (sprite_perso.getPosition().x ==710)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+         }
+          if((abs((sprite_perso.getPosition().x >=650 && sprite_perso.getPosition().x <=720)&& (sprite_perso.getPosition().y ==220)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+         }
+          if((abs((sprite_perso.getPosition().y >=180 && sprite_perso.getPosition().y <=210)&& (sprite_perso.getPosition().x ==650)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+         }
+          if((abs((sprite_perso.getPosition().x >=400 && sprite_perso.getPosition().x <=640)&& (sprite_perso.getPosition().y ==180)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+         }
+          if((abs((sprite_perso.getPosition().y >=150 && sprite_perso.getPosition().y <=180)&& (sprite_perso.getPosition().x ==390)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+         }
+                   if((abs((sprite_perso.getPosition().x >=280 && sprite_perso.getPosition().x <=380)&& (sprite_perso.getPosition().y ==150)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+         }
+                   if((abs((sprite_perso.getPosition().x >=170 && sprite_perso.getPosition().x <=210)&& (sprite_perso.getPosition().y ==150)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+         }
+                   if((abs((sprite_perso.getPosition().y >=270 && sprite_perso.getPosition().y <=390)&& (sprite_perso.getPosition().x ==570)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+         }
+                            if((abs((sprite_perso.getPosition().y >=270 && sprite_perso.getPosition().y <=390)&& (sprite_perso.getPosition().x ==690)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x+10, sprite_perso.getPosition().y);
+         }
+                            if((abs((sprite_perso.getPosition().x >=570 && sprite_perso.getPosition().x <=680)&& (sprite_perso.getPosition().y ==270)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y-10);
+         }
+                                     if((abs((sprite_perso.getPosition().x >=570 && sprite_perso.getPosition().x <=680)&& (sprite_perso.getPosition().y ==390)))&& j==-1){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+         }
+
+
+         //j-2
+                                              if((abs((sprite_perso.getPosition().x >=500 && sprite_perso.getPosition().x <=780)&& (sprite_perso.getPosition().y ==150)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+         }
+                                                       if((abs((sprite_perso.getPosition().y >=90 && sprite_perso.getPosition().y <=150)&& (sprite_perso.getPosition().x ==490)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+         }
+                                                                if((abs((sprite_perso.getPosition().x >=360 && sprite_perso.getPosition().x <=480)&& (sprite_perso.getPosition().y ==90)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+         }
+
+                                                                if((abs((sprite_perso.getPosition().y >=100 && sprite_perso.getPosition().y <=150)&& (sprite_perso.getPosition().x ==350)))&& j==-2){
+
+
+                             sprite_perso.setPosition(sprite_perso.getPosition().x+10, sprite_perso.getPosition().y);
+                                                                }
+
+                                                                if((abs((sprite_perso.getPosition().x >=280 && sprite_perso.getPosition().x <=340)&& (sprite_perso.getPosition().y ==150)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+
 }
+                                                                if((abs((sprite_perso.getPosition().x >=100 && sprite_perso.getPosition().x <=200)&& (sprite_perso.getPosition().y ==150)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+
+}
+  if((abs((sprite_perso.getPosition().y >=160 && sprite_perso.getPosition().y <=510)&& (sprite_perso.getPosition().x ==100)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x+10, sprite_perso.getPosition().y);
+
+}
+  if((abs((sprite_perso.getPosition().x >=220 && sprite_perso.getPosition().x <=770)&& (sprite_perso.getPosition().y ==520)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y-10);
+
+}
+  if((abs((sprite_perso.getPosition().x >=110 && sprite_perso.getPosition().x <=160)&& (sprite_perso.getPosition().y ==520)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y-10);
+
+}
+  if((abs((sprite_perso.getPosition().y >=160 && sprite_perso.getPosition().y <=510)&& (sprite_perso.getPosition().x ==770)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+
+}
+
+if((abs((sprite_perso.getPosition().y >=350 && sprite_perso.getPosition().y <=470)&& (sprite_perso.getPosition().x ==400)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+
+}
+
+  if((abs((sprite_perso.getPosition().x >=400 && sprite_perso.getPosition().x <=650)&& (sprite_perso.getPosition().y ==340)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y-10);
+
+}
+if((abs((sprite_perso.getPosition().y >=350 && sprite_perso.getPosition().y <=470)&& (sprite_perso.getPosition().x ==660)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x+10, sprite_perso.getPosition().y);
+
+}
+
+  if((abs((sprite_perso.getPosition().x >=400 && sprite_perso.getPosition().x <=650)&& (sprite_perso.getPosition().y ==470)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+
+}
+
+if((abs((sprite_perso.getPosition().y >=210 && sprite_perso.getPosition().y <=310)&& (sprite_perso.getPosition().x ==470)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+
+}
+
+  if((abs((sprite_perso.getPosition().x >=470 && sprite_perso.getPosition().x <=660)&& (sprite_perso.getPosition().y ==310)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+
+}
+if((abs((sprite_perso.getPosition().y >=210 && sprite_perso.getPosition().y <=310)&& (sprite_perso.getPosition().x ==670)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x+10, sprite_perso.getPosition().y);
+
+}
+
+  if((abs((sprite_perso.getPosition().x >=470 && sprite_perso.getPosition().x <=660)&& (sprite_perso.getPosition().y ==210)))&& j==-2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y-10);
+
+}
+
+
+
+
+//J=2
+
+ if((abs((sprite_perso.getPosition().x >=620 && sprite_perso.getPosition().x <=730)&& (sprite_perso.getPosition().y ==760)))&& j==2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x, sprite_perso.getPosition().y+10);
+
+}
+ if((abs((sprite_perso.getPosition().y >=710 && sprite_perso.getPosition().y <=810)&& (sprite_perso.getPosition().x ==720)))&& j==2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+
+}
+ if((abs((sprite_perso.getPosition().y >=610 && sprite_perso.getPosition().y <=810)&& (sprite_perso.getPosition().x ==720)))&& j==2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+
+}
+ if((abs((sprite_perso.getPosition().y >=710 && sprite_perso.getPosition().y <=760)&& (sprite_perso.getPosition().x==610)))&& j==2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+}
+
+ if((abs((sprite_perso.getPosition().y >=710 && sprite_perso.getPosition().y <=760)&& (sprite_perso.getPosition().x==610)))&& j==2){
+
+                                               sprite_perso.setPosition(sprite_perso.getPosition().x-10, sprite_perso.getPosition().y);
+}
+   }
---
+++ combat.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include "texture.hpp"
+
+int pokemon=100;
+int potion=10;
+
+void pokecombat(){
+
+
+
+}
